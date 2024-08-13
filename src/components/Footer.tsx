7470f20b66d2e376aa30acd0c4ed92d7c38106d1
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-08-13 23:25:55 +0900

    feature: 레이아웃 ui 처리

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[220px] bg-gray-300">
-      <div className="w-full max-w-[1440px]">Footer</div>
+    <div className="flex justify-center items-center w-full  h-[220px] bg-gray-300">
+      <div className="flex justify-center max-w-[1440px] w-full space-x-12 cursor-pointer text-[18px] font-bold ">
+        <span>Contact Us</span>
+        <span>FAQs</span>
+        <span>Terms of Service</span>
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Wrapper from './Wrapper';
 
 const Header = () => {
   return (
     <div className="sticky top-0 flex items-center justify-center h-20 bg-white">
-      <div className="w-full max-w-[1440px]">
+      <Wrapper>
         <div className="flex justify-between w-full px-8">
-          <div>로고</div>
-          <div>로그인/회원가입</div>
+          <div className="flex items-center space-x-3 cursor-pointer">
+            <div className="bg-gray-200 rounded-full w-8 h-8"></div>
+            <div>이름/닉네임</div>
+          </div>
+          <div className="flex space-x-4 items-center text-[16px] font-bold">
+            <button className="button cursor-pointer" onClick={() => {}}>
+              Home
+            </button>
+            <button className="button cursor-pointer" onClick={() => {}}>
+              About
+            </button>
+            <button className="button cursor-pointer" onClick={() => {}}>
+              Crew
+            </button>
+            <div className="flex items-center border rounded-md py-2 px-3">
+              <input type="text" placeholder="Search in site" className="" />
+              <div>Se</div>
+            </div>
+          </div>
         </div>
-      </div>
+      </Wrapper>
     </div>
   );
 };
