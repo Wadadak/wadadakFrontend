c7ba3dec5dacd5a12a5aa644f892030eeed32013
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-08-14 01:20:51 +0900

    feature: 헤더 디자인 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Wrapper from './Wrapper';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBell,
+  faMagnifyingGlass,
+  faPersonRunning,
+  IconDefinition,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
     <div className="sticky top-0 flex items-center justify-center h-20 bg-white">
       <Wrapper>
         <div className="flex justify-between w-full px-8">
-          <div className="flex items-center space-x-3 cursor-pointer">
-            <div className="bg-gray-200 rounded-full w-8 h-8"></div>
-            <div>이름/닉네임</div>
-          </div>
-          <div className="flex space-x-4 items-center text-[16px] font-bold">
-            <button className="" onClick={() => {}}>
-              Home
-            </button>
-            <button className="" onClick={() => {}}>
-              About
-            </button>
-            <button className="" onClick={() => {}}>
-              Crew
-            </button>
+          {/* 왼쪽 레이아웃 */}
+          <div className="flex items-center space-x-3">
+            <FontAwesomeIcon icon={faPersonRunning} className="w-8 h-8" />
+            {/* 검색창 */}
             <div className="relative flex items-center border rounded-md">
               <input
                 type="text"
-                placeholder="Search in site"
+                placeholder="크루 검색하기"
                 className="py-2 pl-3 pr-8 rounded-md"
               />
               <div
@@ -36,6 +31,17 @@ const Header = () => {
               </div>
             </div>
           </div>
+          {/* 오른쪽 레이아웃 */}
+          <div className="flex space-x-6">
+            <HeaderMenu
+              name={'마이 크루'}
+              icon={faBell}
+              onMenuClick={() => {}}
+            />
+            <HeaderMenu name={'메시지'} icon={faBell} onMenuClick={() => {}} />
+            <HeaderMenu name={'알림'} icon={faBell} onMenuClick={() => {}} />
+            <div className="bg-gray-200 rounded-full w-9 h-9"></div>
+          </div>
         </div>
       </Wrapper>
     </div>
@@ -43,3 +49,21 @@ const Header = () => {
 };
 
 export default Header;
+
+interface HeaderMenuProps {
+  name: string;
+  icon: IconDefinition;
+  onMenuClick: () => void;
+}
+
+const HeaderMenu = ({ name, icon, onMenuClick }: HeaderMenuProps) => {
+  return (
+    <button
+      className="flex flex-col items-center space-y-2"
+      onClick={onMenuClick}
+    >
+      <FontAwesomeIcon icon={icon} className="w-5 h-5" />
+      <div className="text-[10px]">{name}</div>
+    </button>
+  );
+};
