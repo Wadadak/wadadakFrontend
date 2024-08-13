c6525cdce432ca8bbae9c1871dd7dc3622ffa9c7
JUNG HEE CHUL <da.yyy.pa@gmail.com>
2024-08-14 00:53:01 +0900

    feature: login 페이지 개발

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}', // 저희 폴더 구조에 맞게 수정하였습니다! 확인 부탁드립니다! @서율님
     './public/**/*.html',
   ],
@@ -22,6 +22,7 @@ module.exports = {
           accent: '#9fa8d0', // 강조 색상
           neutral: '#2a2e37', // 중립 색상
           'base-100': '#FFFFFF', // 기본 배경 색상
+          'base-200': '#F8F8F8', //회색 배경 색상
 
           info: '#3abff8', // 정보 색상
           success: '#36d399', // 성공 색상
