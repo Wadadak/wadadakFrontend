12814d0d76f31c45ab87be40418869f197cb5c3c
Kim Seo-Yul <yul_the@naver.com>
2024-08-16 11:39:43 +0900

    feat : 메인 컬러와 어울리는 회색 추가, 버튼 컴포넌트 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 
 interface ButtonProps {
-  variant?: '' | 'btn-outline'; // 아웃라인 버튼으로 변경, ''는 배경 있는 버튼
-  color?: 'btn-primary' | 'btn-secondary' | 'btn-ghost';
-  textColor?: 'text-white' | 'text-black' | 'text-gray-500';
-  size?: 'btn-lg' | 'btn-md' | 'btn-sm' | 'btn-xs';
+  outline?: boolean; // 아웃라인 버튼으로 변경
+  color?: 'primary' | 'secondary' | 'accent' | string;
+  textColor?: 'white' | 'black' | string;
+  size?: 'lg' | 'md' | 'sm' | 'xs';
   disabled?: boolean;
   onClick?: () => void;
   children: React.ReactNode;
 }
 
 const Button = ({
-  variant = '',
-  color = 'btn-primary',
-  textColor = 'text-white',
-  size = 'btn-md',
+  outline = false,
+  color = 'primary',
+  textColor = 'white',
+  size = 'md',
   disabled = false,
   onClick,
   children,
 }: ButtonProps) => {
+  const outlineClass = outline ? `btn-outline` : '';
+  const colorClass = `btn-${color}`;
+  const textColorClass = `text-${textColor}`;
+  const sizeClass = `btn-${size}`;
+
   return (
     <button
-      className={`btn hover:btn-accent hover:${textColor} ${variant} ${color} ${size} ${textColor}`}
+      className={`btn ${outlineClass} ${colorClass} ${sizeClass} ${textColorClass}`}
       disabled={disabled}
       onClick={onClick}
     >
