1ee7698075651470ae0e309ea10ce70f4e834dfb
kimsyul <yul_the@naver.com>
2024-08-20 21:27:08 +0900

    feat : 라우팅 설정

--- src/components/crew/MemberList.tsx
+++ src/components/crew/MemberList.tsx
@@ -2,14 +2,31 @@ import React from 'react';
 import Button from '../common/Button';
 import Avatar from '../common/Avatar';
 import { CrewMembers } from '@/types/memberTypes';
+import { useRouter } from 'next/navigation';
 
 interface MemberListProps {
   members: CrewMembers;
 }
 
 const MemberList = ({ members }: MemberListProps) => {
+  const router = useRouter();
+
+  // TODO url 변경
+  const handleProfileClick = () => {
+    router.push(`/profile`);
+  };
+
+  // TODO url 변경
+  const handleChatClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    router.push(`/chat`);
+  };
+
   return (
-    <div className="grid gap-4 md:grid-cols-2">
+    <div
+      className="grid gap-4 md:grid-cols-2 cursor-pointer"
+      onClick={handleProfileClick}
+    >
       {members.map((member) => (
         <div
           key={member.id}
@@ -19,9 +36,11 @@ const MemberList = ({ members }: MemberListProps) => {
             <Avatar src={member.avatar} alt={member.name} />
             <span className="font-bold">{member.name}</span>
           </div>
-          <Button size="sm" color="secondary">
-            1:1 채팅
-          </Button>
+          <div className="flex items-center gap-2">
+            <Button size="sm" color="secondary" onClick={handleChatClick}>
+              채팅
+            </Button>
+          </div>
         </div>
       ))}
     </div>
