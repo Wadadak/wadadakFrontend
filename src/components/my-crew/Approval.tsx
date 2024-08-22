2248d9238ad07c7845226d677fa83aada4e65cd3
Kim Seo-Yul <yul_the@naver.com>
2024-08-22 10:50:51 +0900

    feat : 승인 거절 모달 구현

--- src/components/my-crew/Approval.tsx
+++ src/components/my-crew/Approval.tsx
@@ -1,23 +1,65 @@
-import React, { useState } from 'react';
+'use client';
+
+import React from 'react';
 import AllMemberList from '@/components/my-crew/AllMemberList';
 import Button from '@/components/common/Button';
+import SimpleModal from '../common/SimpleModal';
 import { mockCrewMembers } from '@/mocks/mockData/mockCrewMembers';
+import useModal from '@/hooks/useModal';
 
 const Approval = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const handleOpenModal = (e: React.MouseEvent) => {
-    e.stopPropagation();
-    setIsModalOpen(true);
-  };
-  const handleCloseModal = () => setIsModalOpen(false);
+  const approvalModal = useModal();
+  const rejectionModal = useModal();
 
   return (
-    <AllMemberList members={mockCrewMembers}>
-      <Button size="sm" color="secondary">
-        승인
-      </Button>
-      <Button size="sm">거절</Button>
-    </AllMemberList>
+    <>
+      <AllMemberList members={mockCrewMembers}>
+        <Button
+          size="sm"
+          color="secondary"
+          onClick={approvalModal.handleOpenModal}
+        >
+          승인
+        </Button>
+        <Button size="sm" onClick={rejectionModal.handleOpenModal}>
+          거절
+        </Button>
+      </AllMemberList>
+      {approvalModal.isModalOpen && (
+        <SimpleModal
+          isOpen={approvalModal.isModalOpen}
+          onClose={approvalModal.handleCloseModal}
+          title="정말 승인하시겠습니까?"
+        >
+          <div className="flex justify-end gap-2">
+            <Button color="accent">예</Button>
+            <Button
+              color="accent"
+              outline
+              onClick={approvalModal.handleCloseModal}
+            >
+              아니요
+            </Button>
+          </div>
+        </SimpleModal>
+      )}
+      {rejectionModal.isModalOpen && (
+        <SimpleModal
+          isOpen={rejectionModal.isModalOpen}
+          onClose={rejectionModal.handleCloseModal}
+          title="정말 거절하시겠습니까?"
+        >
+          <div className="flex justify-end gap-2">
+            <Button outline color="accent">
+              예
+            </Button>
+            <Button color="accent" onClick={rejectionModal.handleCloseModal}>
+              아니요
+            </Button>
+          </div>
+        </SimpleModal>
+      )}
+    </>
   );
 };
 
