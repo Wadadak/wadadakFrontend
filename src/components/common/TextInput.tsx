401473e9c7e6e5dd3b2564c207352c0b1679c929
Kim Seo-Yul <yul_the@naver.com>
2024-08-22 15:00:22 +0900

    feat : 유효성 검사에서 에러 떴을 때 ui 추가

--- src/components/common/TextInput.tsx
+++ src/components/common/TextInput.tsx
@@ -10,6 +10,7 @@ interface TextInputProps {
   width?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | string; // 너비를 위한 프롭
   as?: 'input' | 'textarea'; //input 또는 textarea 선택 (기본은 input)
   rows?: number; // textarea의 경우
+  error?: string; // 에러 메시지 프롭스
 }
 
 const TextInput = ({
@@ -22,30 +23,36 @@ const TextInput = ({
   width,
   as = 'input',
   rows = 5,
+  error,
 }: TextInputProps) => {
   const inputWidthClass = width ? `max-w-${width}` : 'max-w-xs';
   const textareaWidthClass = width ? `w-${width}` : 'w-1/2';
 
-  return as === 'textarea' ? (
-    <textarea
-      className={`textarea textarea-bordered ${textareaWidthClass}`}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder}
-      maxLength={maxLength}
-      required={required}
-      rows={rows}
-    />
-  ) : (
-    <input
-      className={`input input-bordered w-full ${inputWidthClass}`}
-      type={type}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder={placeholder}
-      required={required}
-      maxLength={maxLength}
-    />
+  return (
+    <>
+      {as === 'textarea' ? (
+        <textarea
+          className={`textarea textarea-bordered ${textareaWidthClass} ${error && 'textarea-error'}`}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+          maxLength={maxLength}
+          required={required}
+          rows={rows}
+        />
+      ) : (
+        <input
+          className={`input input-bordered w-full ${inputWidthClass} ${error && 'input-error'}`}
+          type={type}
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+          placeholder={placeholder}
+          required={required}
+          maxLength={maxLength}
+        />
+      )}
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </>
   );
 };
 
