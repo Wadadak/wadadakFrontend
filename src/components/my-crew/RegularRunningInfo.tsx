fc75dd2565d02fd8f95db0bffed8a407e0aa8d16
Kim Seo-Yul <yul_the@naver.com>
2024-08-23 11:18:15 +0900

    Feat : 정기 러닝 등록, 수정 진행 중

--- src/components/my-crew/RegularRunningInfo.tsx
+++ src/components/my-crew/RegularRunningInfo.tsx
@@ -9,18 +9,7 @@ import CheckBox from '../common/CheckBox';
 import Dropdown from '../common/Dropdown';
 import DatePicker from 'react-datepicker';
 import Label from '../common/Label';
-
-const RegularRunningInfo = () => {
-  const { crewId } = useParams();
-  const id = parseInt(crewId as string, 10);
-  const crew = mockCrewList.find((crew) => crew.crewId === id);
-
-  const [runningInfo, setRunningInfo] = useState<RegularRunningInfo[]>(
-    crew?.regularRunningInfo || [],
-  );
-
-  return <div>RegularRunningInfo</div>;
-};
+import { mockActivityRegions } from '@/mocks/mockData/mockActivityRegions';
 
 interface RunningInfoFormProps {
   initialInfo: RegularRunningInfo | null;
@@ -30,28 +19,29 @@ interface RunningInfoFormProps {
 
 // 주기 옵션
 const frequencyOptions = [
-  { value: 1, label: '1주' },
-  { value: 2, label: '2주' },
-  { value: 3, label: '3주' },
-  { value: 4, label: '4주' },
-  { value: 5, label: '5주' },
-  { value: 6, label: '6주' },
-  { value: 7, label: '7주' },
-  { value: 8, label: '8주' },
-  { value: 9, label: '9주' },
-  { value: 10, label: '10주' },
-  { value: 11, label: '11주' },
-  { value: 12, label: '12주' },
+  { id: 1, name: '1주' },
+  { id: 2, name: '2주' },
+  { id: 3, name: '3주' },
+  { id: 4, name: '4주' },
+  { id: 5, name: '5주' },
+  { id: 6, name: '6주' },
+  { id: 7, name: '7주' },
+  { id: 8, name: '8주' },
+  { id: 9, name: '9주' },
+  { id: 10, name: '10주' },
+  { id: 11, name: '11주' },
+  { id: 12, name: '12주' },
+  { id: 0, name: '기타' },
 ];
 
 const timesOptions = [
-  { value: 1, label: '1번' },
-  { value: 2, label: '2번' },
-  { value: 3, label: '3번' },
-  { value: 4, label: '4번' },
-  { value: 5, label: '5번' },
-  { value: 6, label: '6번' },
-  { value: 7, label: '7번' },
+  { id: 1, name: '1번' },
+  { id: 2, name: '2번' },
+  { id: 3, name: '3번' },
+  { id: 4, name: '4번' },
+  { id: 5, name: '5번' },
+  { id: 6, name: '6번' },
+  { id: 7, name: '7번' },
 ];
 
 // 요일 옵션
@@ -80,6 +70,7 @@ const RunningInfoForm = ({
     initialInfo?.time ? new Date(initialInfo.time) : null,
   );
 
+  // 요일 선택
   const handleWeekdayChange = (day: string) => {
     setWeekdays((prev) => {
       if (prev.includes(day)) {
@@ -103,11 +94,22 @@ const RunningInfoForm = ({
 
   return (
     <>
-      <Label label="빈도">
-        <div className="flex flex-row space-x-4">
-          <Dropdown placeholder="주기" onChange={(e) => setWeeks(e)} />
-          <Dropdown placeholder="횟수" />
-        </div>
+      <p className="text-center text-3xl">
+        {initialInfo ? '정기 러닝 정보 수정' : '정기 러닝 정보 추가'}
+      </p>
+      <Label label="활동 지역">
+        <Dropdown
+          options={mockActivityRegions}
+          placeholder="활동 지역"
+          onChange={(id) => setLocation(value as string)}
+        />
+      </Label>
+      <Label label="주기">
+        <Dropdown
+          options={frequencyOptions}
+          placeholder="주기"
+          onChange={(value) => setWeeks(value as number)}
+        />
       </Label>
     </>
   );
