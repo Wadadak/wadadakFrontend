f42d94a267b1d89ab80d3305f737f4b65341560e
Kim Seo-Yul <yul_the@naver.com>
2024-08-23 13:45:15 +0900

    feat : ui 변경, 로직 변경

--- src/components/common/TimePicker.tsx
+++ src/components/common/TimePicker.tsx
@@ -6,12 +6,19 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import Button from '@/components/common/Button';
 
-const TimePicker: React.FC = () => {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+interface TimePickerProps {
+  selectedTimes: string[];
+  onTimeChange: (times: string[]) => void;
+}
+
+const TimePicker = ({ selectedTimes, onTimeChange }: TimePickerProps) => {
   const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
 
-  const handleDateChange = (date: Date | null) => {
-    setSelectedDate(date);
+  const handleTimeChange = (time: Date | null) => {
+    if (time) {
+      const newTime = time.toISOString().substring(11, 16); // "HH:mm" 형식
+      onTimeChange([...selectedTimes, newTime]);
+    }
     setIsDatePickerOpen(false); // 날짜 선택 후 피커를 닫음
   };
 
@@ -25,14 +32,37 @@ const TimePicker: React.FC = () => {
       {isDatePickerOpen && (
         <div className="absolute z-10 mt-2">
           <DatePicker
-            selected={selectedDate}
-            onChange={handleDateChange}
+            selected={null}
+            onChange={handleTimeChange}
             showTimeSelect
             timeIntervals={15}
-            dateFormat="yyyy/MM/dd h:mm aa"
+            timeCaption="Time"
+            dateFormat="HH:mm"
             className="p-2 border border-gray-300 rounded shadow-lg"
             inline // DatePicker를 인라인으로 표시
           />
+        </div>
+      )}
+
+      {selectedTimes.length > 0 && (
+        <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
+          {selectedTimes.map((time, index) => (
+            <div
+              key={index}
+              className="card bg-base-100 shadow-md rounded-lg p-4 flex justify-between items-center"
+            >
+              <span className="font-medium text-lg">{time}</span>
+              <Button
+                size="sm"
+                color="accent"
+                onClick={() =>
+                  onTimeChange(selectedTimes.filter((_, i) => i !== index))
+                }
+              >
+                삭제
+              </Button>
+            </div>
+          ))}
         </div>
       )}
     </div>
