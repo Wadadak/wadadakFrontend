9a47d5121e537f0ffcb6132f4ada5f69f3f6784c
kimsyul <yul_the@naver.com>
2024-08-23 22:46:50 +0900

    feat : TimePicker 에러 ui 추가

--- src/components/common/TimePicker.tsx
+++ src/components/common/TimePicker.tsx
@@ -8,9 +8,14 @@ import Button from './Button';
 interface TimePickerProps {
   onTimeChange: (time: string | null) => void;
   initialTime?: string | null;
+  error?: string;
 }
 
-const TimePicker = ({ onTimeChange, initialTime = null }: TimePickerProps) => {
+const TimePicker = ({
+  onTimeChange,
+  initialTime = null,
+  error,
+}: TimePickerProps) => {
   const [selectedTime, setSelectedTime] = useState<string | null>(
     initialTime || null,
   );
@@ -48,7 +53,7 @@ const TimePicker = ({ onTimeChange, initialTime = null }: TimePickerProps) => {
           timeIntervals={30}
           timeCaption="Time"
           dateFormat="HH:mm"
-          className="input input-bordered max-w-xs"
+          className={`input input-bordered max-w-xs ${error && 'textarea-error'}`}
           placeholderText="시간을 선택하세요"
           open={isDatePickerOpen} // 드롭다운 상태
           onClickOutside={() => setIsDatePickerOpen(false)} // 드롭다운 외부 클릭 시 닫기
@@ -65,6 +70,7 @@ const TimePicker = ({ onTimeChange, initialTime = null }: TimePickerProps) => {
           </Button>
         </div>
       )}
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
     </div>
   );
 };
