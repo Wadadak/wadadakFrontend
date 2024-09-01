5280fec7e4d7a58467d29d0b02440186a74fd594
hee-chul <da.yyy.pa@gmail.com>
2024-09-01 11:56:12 +0900

    feat: 회원가입 api 연결

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,44 +3,51 @@
 import Avatar from '@/components/common/Avatar';
 import CheckBox from '@/components/common/CheckBox';
 import Dropdown, { DropdownOption } from '@/components/common/Dropdown';
+import ImageUpload from '@/components/common/ImageUpload';
+import Label from '@/components/common/Label';
 import TextInput from '@/components/common/TextInput';
 import { ToggleButton } from '@/components/common/ToggleButtion';
 import YearOfBirthDropdown from '@/components/common/YearOfBirthDropdown';
 import { TitleBanner } from '@/components/layout/TitleBanner';
+import { useSignup } from '@/hooks/user/useSignup';
+import { useRegions } from '@/hooks/useRegions';
 import { loginState } from '@/recoil/atoms/userState';
+import { genderType, SignupRequest } from '@/types/userTypes';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 
 const SignUpPage = () => {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [name, setName] = useState<string>('');
-  const [nickName, setNickName] = useState<string>('');
-  const [profileImage, setProfileImage] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const [email, setEmail] = useState<string>();
+  const [password, setPassword] = useState<string>();
+  const [confirmPassword, setConfirmPassword] = useState<string>();
+  const [name, setName] = useState<string>();
+  const [nickName, setNickName] = useState<string>();
+  const [profileImage, setProfileImage] = useState<File>();
+  const [phoneNumber, setPhoneNumber] = useState<string>();
   const [gender, setGender] = useState<string[]>([]);
-  const [birthYear, setBirthYear] = useState<number>(0);
-  const [activityRegion, setActivityRegion] = useState<string>('');
+  const [birthYear, setBirthYear] = useState<number | undefined>();
+  const [activityRegion, setActivityRegion] = useState<string>();
 
   const router = useRouter();
   const [isNameOn, setIsNameOn] = useState<boolean>(false);
-  const [isProfileOn, setIsProfileOn] = useState<boolean>(false);
   const [isPhoneNumberOn, setIsPhoneNumberOn] = useState<boolean>(false);
   const [isGenderOn, setIsGenderOn] = useState<boolean>(false);
   const [isAgeOn, setIsAgeOn] = useState<boolean>(false);
 
   const setLogin = useSetRecoilState(loginState);
+  const regionList = useRegions().data;
 
-  const validateEmail = (email: string): boolean => {
+  const validateEmail = (email?: string): boolean => {
     //기획: email 형식(xxx@xxx.xxx), 50자 이내
+    if (!email) return false;
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
-  const validatePassword = (password: string): boolean => {
+  const validatePassword = (password?: string): boolean => {
     //기획: 숫자, 대소문자, 특수문자 1개 이상, 8자~50자
+    if (!password) return false;
     const passwordRegex =
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_\-+=<>?{}[\]~])[A-Za-z\d!@#$%^&*()_\-+=<>?{}[\]~]{8,50}$/;
     return passwordRegex.test(password);
@@ -53,7 +60,7 @@ const SignUpPage = () => {
       return;
     }
 
-    if (email.length > 50) {
+    if (email && email?.length > 50) {
       alert('이메일은 50자 이내여야 합니다.');
       return;
     }
@@ -70,34 +77,44 @@ const SignUpPage = () => {
       return;
     }
 
-    const body = {
-      username: name,
-      email: email,
-      password: password,
-      checkPassword: confirmPassword,
-      nickname: nickName,
+    const body: SignupRequest = {
+      name,
+      email,
+      password,
+      confirmPassword,
+      nickName,
       phoneNumber,
       profileImage,
-      gender: gender[0],
+      gender: gender[0] as genderType,
       birthYear,
       activityRegion,
-
-      // hctodo API 명세 추가되면 다시 수정할 것.
-      isNameOn,
-      isProfileOn,
-      isPhoneNumberOn,
-      isGenderOn,
-      isAgeOn,
+      nameVisibility: isNameOn ? 'PUBLIC' : 'PRIVATE',
+      phoneNumberVisibility: isPhoneNumberOn ? 'PUBLIC' : 'PRIVATE',
+      genderVisibility: isGenderOn ? 'PUBLIC' : 'PRIVATE',
+      birthYearVisibility: isAgeOn ? 'PUBLIC' : 'PRIVATE',
     };
 
-    console.log('SignUpPage body', body);
-    alert('회원가입이 완료되었습니다.');
-    setLogin(true);
-    router.push('/');
+    signup(body);
+
+    // console.log('SignUpPage body', body);
+    // alert('회원가입이 완료되었습니다.');
+    // setLogin(true);
+    // router.push('/');
 
     //api call
     // executeMutation(body);
   };
+
+  const { mutate: signup } = useSignup(
+    () => {
+      alert('회원가입이 완료되었습니다.');
+      setLogin(true);
+      router.push('/');
+    },
+    (message) => {
+      alert(message);
+    },
+  );
 
   return (
     <div className="flex flex-col bg-base-200">
@@ -108,8 +125,7 @@ const SignUpPage = () => {
             <div className="text-[20px] font-bold text-gray-700">회원가입</div>
             <div className="flex flex-col mt-5 space-y-4">
               {/* e-mail */}
-              <div className="form-control">
-                <Title title={'이메일'} htmlFor={'email'} required={true} />
+              <Label label={'이메일'} textSize="sm" required>
                 <TextInput
                   value={email}
                   onChange={(value) => {
@@ -117,170 +133,133 @@ const SignUpPage = () => {
                   }}
                   placeholder="이메일 입력"
                 />
-              </div>
+              </Label>
               {/* password */}
-              <div className="form-control">
-                <Title
-                  title={'비밀번호'}
-                  htmlFor={'password'}
-                  required={true}
-                />
-                <input
-                  type="password"
-                  id="password"
-                  className="input input-bordered"
-                  placeholder="비밀번호 입력"
+              <Label label={'비밀번호'} textSize="sm" required>
+                <TextInput
                   value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="form-control">
-                <Title
-                  title={'비밀번호 확인'}
-                  htmlFor={'confirmPassword'}
-                  required={true}
-                />
-                <input
                   type="password"
-                  id="confirmPassword"
-                  className="input input-bordered"
-                  placeholder="비밀번호 확인 입력"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  onChange={setPassword}
+                  placeholder="비밀번호 입력"
                 />
-              </div>
+              </Label>
+              <Label label={'비밀번호 확인'} textSize="sm" required>
+                <TextInput
+                  value={confirmPassword}
+                  type="password"
+                  onChange={setConfirmPassword}
+                  placeholder="비밀번호 확인 입력"
+                />
+              </Label>
               {/* name */}
-              <div className="flex flex-col space-y-3">
-                <div className="form-control">
-                  <Title title={'이름'} htmlFor={'name'} required={true} />
+              <Label label={'이름'} textSize="sm" required>
+                <div className="flex flex-col space-y-3">
                   <TextInput
                     value={name}
-                    onChange={(value) => {
-                      setName(value);
-                    }}
+                    onChange={setName}
                     placeholder="이름을 입력해 주세요"
                   />
-                </div>
-                <div className="flex items-center space-x-2">
-                  <div className="text-[12px] font-semibold">프로필 공개</div>
-                  <ToggleButton
-                    onButtonClick={() => setIsNameOn(!isNameOn)}
-                    isOn={isNameOn}
-                  />
-                </div>
-              </div>
-              {/* nickName */}
-              <div className="form-control">
-                <Title title={'닉네임'} htmlFor={'nickName'} required={true} />
-                <TextInput
-                  value={nickName}
-                  onChange={(value) => {
-                    setNickName(value);
-                  }}
-                  placeholder="닉네임을 입력해 주세요"
-                />
-              </div>
-              {/* P.image */}
-              <div className="flex items-center space-x-5">
-                <div className="flex flex-col items-center space-y-2">
-                  <Title
-                    title={'프로필 이미지'}
-                    htmlFor={'profileImage'}
-                    required={true}
-                  />
-                  <Avatar size="w-24" />
                   <div className="flex items-center space-x-2">
                     <div className="text-[12px] font-semibold">프로필 공개</div>
                     <ToggleButton
-                      onButtonClick={() => setIsProfileOn(!isProfileOn)}
-                      isOn={isProfileOn}
+                      isOn={isNameOn}
+                      onButtonClick={() => setIsNameOn(!isNameOn)}
                     />
                   </div>
                 </div>
-                <button className="btn btn-outline btn-secondary btn-sm">
-                  이미지 업로드
-                </button>
-              </div>
-              {/* phoneNumber */}
-              <div className="flex flex-col space-y-3">
-                <div className="form-control">
-                  <Title
-                    title={'휴대전화'}
-                    htmlFor={'phoneNumber'}
-                    required={true}
+              </Label>
+              {/* nickName */}
+              <Label label={'닉네임'} textSize="sm" required>
+                <TextInput
+                  value={nickName}
+                  onChange={setNickName}
+                  placeholder="닉네임을 입력해 주세요"
+                />
+              </Label>
+              {/* P.image */}
+              <Label label={'프로필 이미지'} textSize="sm">
+                <div className="flex items-center space-x-5">
+                  <Avatar size="w-24" />
+                  <ImageUpload
+                    onImageChange={function (file?: File): void {
+                      throw new Error('Function not implemented.');
+                    }}
                   />
+                </div>
+              </Label>
+              {/* phoneNumber */}
+              <Label label={'휴대전화'} textSize="sm" required>
+                <div className="flex flex-col space-y-3">
                   <TextInput
                     value={phoneNumber}
-                    onChange={(value) => {
-                      setPhoneNumber(value);
-                    }}
+                    onChange={setPhoneNumber}
                     placeholder="번호를 입력해 주세요"
                   />
+                  <div className="flex items-center space-x-2">
+                    <div className="text-[12px] font-semibold">프로필 공개</div>
+                    <ToggleButton
+                      isOn={isPhoneNumberOn}
+                      onButtonClick={() => setIsPhoneNumberOn(!isPhoneNumberOn)}
+                    />
+                  </div>
                 </div>
-                <div className="flex items-center space-x-2">
-                  <div className="text-[12px] font-semibold">프로필 공개</div>
-                  <ToggleButton
-                    onButtonClick={() => setIsPhoneNumberOn(!isPhoneNumberOn)}
-                    isOn={isPhoneNumberOn}
-                  />
-                </div>
-              </div>
+              </Label>
               {/* gender */}
-              <div className="flex flex-col space-y-3">
-                <div className="flex flex-col space-y-3">
-                  <Title title={'성별'} htmlFor={''} required={true} />
+              <Label label={'성별'} textSize="sm" required>
+                <div className="flex flex-col space-y-5">
                   <CheckBox
                     multiple={false}
                     options={[
                       {
-                        id: 'M',
+                        id: 'MALE',
                         name: '남자',
                       },
                       {
-                        id: 'F',
+                        id: 'FEMALE',
                         name: '여자',
                       },
                     ]}
                     selectedValues={gender}
                     onChange={(list) => {
-                      console.log(list);
-                      setGender(list);
+                      setGender(list as string[]);
                     }}
                   />
+                  <div className="flex items-center space-x-2">
+                    <div className="text-[12px] font-semibold">프로필 공개</div>
+                    <ToggleButton
+                      onButtonClick={() => setIsGenderOn(!isGenderOn)}
+                      isOn={isGenderOn}
+                    />
+                  </div>
                 </div>
-                <div className="flex items-center space-x-2">
-                  <div className="text-[12px] font-semibold">프로필 공개</div>
-                  <ToggleButton
-                    onButtonClick={() => setIsGenderOn(!isGenderOn)}
-                    isOn={isGenderOn}
-                  />
-                </div>
-              </div>
+              </Label>
               {/* age */}
-              <div className="flex flex-col space-y-1">
-                <Title title={'나이'} htmlFor={''} required={true} />
-                <YearOfBirthDropdown
-                  selectedYear={birthYear}
-                  onYearChange={setBirthYear}
-                />
-                <div className="flex items-center space-x-2">
-                  <div className="text-[12px] font-semibold">프로필 공개</div>
-                  <ToggleButton
-                    onButtonClick={() => setIsAgeOn(!isAgeOn)}
-                    isOn={isAgeOn}
+              <Label label={'나이'} textSize="sm" required>
+                <div className="flex flex-col space-y-3">
+                  <YearOfBirthDropdown
+                    placeholder="태어난 해를 선택해 주세요"
+                    selectedYear={birthYear}
+                    onYearChange={setBirthYear}
                   />
+                  <div className="flex items-center space-x-2">
+                    <div className="text-[12px] font-semibold">프로필 공개</div>
+                    <ToggleButton
+                      onButtonClick={() => setIsAgeOn(!isAgeOn)}
+                      isOn={isAgeOn}
+                    />
+                  </div>
                 </div>
-              </div>
+              </Label>
               {/* activity area */}
-              <div>
-                <Title title={'활동지역'} htmlFor={''} required={true} />
+              <Label label={'활동지역'} textSize="sm">
                 <Dropdown
-                  options={regionList}
+                  placeholder="지역을 선택해 주세요"
+                  options={regionList as DropdownOption[]}
                   onChange={(region) => {
                     setActivityRegion(region as string);
                   }}
                 />
-              </div>
+              </Label>
             </div>
             {/* sign up */}
             <div className="mt-8 form-control">
@@ -310,3 +289,24 @@ const Title = ({ title, htmlFor, required }: TitleProps) => {
     </label>
   );
 };
+
+
+// {
+//   "id": 20,
+//   "email": "aaa@gmail.com",
+//   "emailVerified": false,
+//   "phoneNumber": "01012341235",
+//   "name": "정희철",
+//   "nickName": "정희철테스트1",
+//   "birthYear": 2014,
+//   "gender": "MALE",
+//   "roles": [
+//       "ROLE_USER"
+//   ],
+//   "activityRegion": "NATIONWIDE",
+//   "imageUrl": "https://running-service.s3.ap-northeast-2.amazonaws.com/user-default",
+//   "nameVisibility": "PUBLIC",
+//   "phoneNumberVisibility": "PUBLIC",
+//   "genderVisibility": "PUBLIC",
+//   "birthYearVisibility": "PUBLIC"
+// }
